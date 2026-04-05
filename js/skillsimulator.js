b9be132f19957abb2e768b9fbe06b4d63f9ef560
Kholdy776 <95378285+Kholdy776@users.noreply.github.com>
2026-04-04 17:51:33 -0700

    Update skillsimulator.js

--- js/skillsimulator.js
+++ js/skillsimulator.js
@@ -714,7 +714,7 @@ var SkillSimulator = {
 
         // Create chevron (">" shape centered at 0,0)
         var chevron = document.createElementNS(ns, "polyline");
-        chevron.setAttribute("points", "4,-2 0,2 -4,-2");
+        chevron.setAttribute("points", "-2,-4 2,0 -2,4");
         chevron.setAttribute("fill", "none");
         chevron.setAttribute("stroke", "black");
         chevron.setAttribute("stroke-width", 2);
